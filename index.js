20cfd1e2befc51b02801e897cb976a0bc276cc80
petermriya <petermuriya@gmail.com>
2022-08-22 06:13:10 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide (){
+let num1 = 2000;
+let num2 = 100;
+return num1 / num2;
+}
+let square = (num1) =>{
+    return num1 **2;
+}
+let add = (num1, num2) =>{
+    return num1 + num2;
+}
